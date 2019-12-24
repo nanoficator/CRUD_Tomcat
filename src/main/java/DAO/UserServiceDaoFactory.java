ea6ba6f0fcf616ccf943616c78f594bbe5bf8f85
Admin <mkuzivanov@gmail.com>
2019-12-24 23:36:53 +0300

    CRUD

--- src/main/java/DAO/UserServiceDaoFactory.java
+++ src/main/java/DAO/UserServiceDaoFactory.java
@@ -9,17 +9,16 @@ import java.util.Properties;
 
 public class UserServiceDaoFactory {
 
-    public static UserServiceDao getDao() throws DBException {
-        try {
 
-            FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Mikhail Kuzivanov\\IdeaProjects\\CRUD_Tomcat\\src\\main\\resources\\config.properties");
+    public static UserServiceDao getDao() throws DBException {
+        try (FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Admin\\IdeaProjects\\CRUD_Tomcat\\src\\main\\resources\\config.properties")) {
             Properties properties = new Properties();
             properties.load(fileInputStream);
             String daoType = properties.getProperty("DAOType");
 
-            if (daoType.toLowerCase().equals("hql")) {
+            if (daoType.equalsIgnoreCase("hql")) {
                 return new UserServiceHqlDao(DBHelper.getInstance().getConfiguration());
-            } else if (daoType.toLowerCase().equals("sql")) {
+            } else if (daoType.equalsIgnoreCase("sql")) {
                 return new UserServiceSqlDao(DBHelper.getInstance().getConnection());
             }
 
