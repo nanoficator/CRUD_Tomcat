e0be15fb9ebdcd76566c80498a4e0b9ad44a6d11
Mikhail Kuzivanov <mkuzivanov@gmail.com>
2020-01-10 11:28:08 +0300

    CRUD

--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -1,6 +1,30 @@
 package security;
 
+import java.util.*;
+
 public class SecurityConfig {
+
     public static final String roleAdmin = "admin";
     public static final String roleUser = "user";
+
+    private static final Map<String, List<String>> mapConfig = new HashMap<>();
+
+    public static void init() {
+        List<String> urlPatternsAdmin = new ArrayList<>();
+        urlPatternsAdmin.add("/admin");
+
+        List<String> urlPatternsUser = new ArrayList<>();
+        urlPatternsUser.add("/info");
+
+        mapConfig.put(roleAdmin, urlPatternsAdmin);
+        mapConfig.put(roleUser, urlPatternsUser);
+    }
+
+    public static Set<String> getAllRoles() {
+        return mapConfig.keySet();
+    }
+
+    public static List<String> getUrlPatternsForRole(String role) {
+        return mapConfig.get(role);
+    }
 }
---
+++ src/main/java/security/SecurityUtil.java
@@ -0,0 +1,40 @@
+package security;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Set;
+
+public class SecurityUtil {
+
+    // Проверить требует ли данный 'request' входа в систему или нет.
+    public static boolean isSecurityPage(HttpServletRequest request) {
+        String urlPattern = UrlPatternUtil.getUrlPattern(request);
+
+        Set<String> roles = SecurityConfig.getAllRoles();
+
+        for (String role : roles) {
+            List<String> urlPatterns = SecurityConfig.getUrlPatternsForRole(role);
+            if (urlPatterns != null && urlPatterns.contains(urlPattern)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasPermission(HttpServletRequest request) {
+        String urlPattern = UrlPatternUtil.getUrlPattern(request);
+
+        Set<String> allRoles = SecurityConfig.getAllRoles();
+
+        for (String role : allRoles) {
+            if (!request.isUserInRole(role)) {
+                continue;
+            }
+            List<String> urlPatterns = SecurityConfig.getUrlPatternsForRole(role);
+            if (urlPatterns != null && urlPatterns.contains(urlPattern)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
